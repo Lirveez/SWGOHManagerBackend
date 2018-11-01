d710bb1091eaab14732966cc11b40d6e8124117d
Lirveez <lirveez@gmail.com>
2018-11-01 22:16:50 +0300

    Update guild's members data

---
+++ src/main/java/commands/UpdateGuildCommand.java
@@ -0,0 +1,24 @@
+package commands;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import services.GetResponse;
+import services.MySQLTools;
+import services.Profile;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class UpdateGuildCommand implements AjaxCommand {
+    @Override
+    public Object doCommand(Map parameters) throws IOException {
+        int guild_id =  Integer.parseInt(((String[]) parameters.get("guild_id"))[0]);
+        JSONObject jsonObject = new JSONObject(GetResponse.getResponse("https://swgoh.gg/api/guild/"+guild_id+"/"));
+        jsonObject.put("guild_id",guild_id);
+       /* Profile[] guild = new Profile[players.length()];
+        for (int i=0;i<players.length();i++){
+            guild[i] = new Profile(players.getJSONObject(i));
+        }*/
+        return "Success. Time spend: "+MySQLTools.insertData(MySQLTools.GUILD_PLAYERS,jsonObject.toString())+"ms";
+    }
+}
